4c91751f35cb0edfcacc92fcd971543439f1b26f
zzde <zude1995@126.com>
2025-03-27 20:30:11 +0800

    提交描述信息（如：Initial commit）

---
+++ PTP/ptp/ptp.cache/ip/2020.2.2/650e096ae9f68f27/mult_gen_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.2.2 (win64) Build 3118627 Tue Feb  9 05:14:06 MST 2021
+-- Date        : Fri May 31 16:15:56 2024
+-- Host        : LAPTOP-MSJTU7NL running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ mult_gen_0_stub.vhdl
+-- Design      : mult_gen_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xcku035-fbva676-2-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    CLK : in STD_LOGIC;
+    A : in STD_LOGIC_VECTOR ( 47 downto 0 );
+    P : out STD_LOGIC_VECTOR ( 63 downto 0 )
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "CLK,A[47:0],P[63:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "mult_gen_v12_0_16,Vivado 2020.2.2";
+begin
+end;
